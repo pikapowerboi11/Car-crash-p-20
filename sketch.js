06959bbd7773d9d1d671e8d7cdde127b9d43c86e
pikapowerboi11 <73790637+pikapowerboi11@users.noreply.github.com>
2020-11-20 15:45:12 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var car, wall;
+var weight, speed;
+
+function setup() {
+  createCanvas(1600,400);
+  
+
+  speed=random(55,90)
+  weight=random(400,1500)
+
+  car=createSprite(50, 200, 50, 50);
+
+  car.velocityX = speed
+  wall=createSprite(1200,200,50,height/2)
+  wall.shapeColor = color(80,80,80)
+
+}
+
+function draw() {
+  background(255,255,255);  
+ 
+
+  if(wall.x-car.x < (car.width+wall.width)/2){
+    car.velocityX = 0
+    var defor=0.5 * weight * speed* speed/22500;
+    if(defor<100){
+      car.shapeColor = color(0, 225, 0)
+  }
+
+  if(defor<180 && defor>100){
+    car.shapeColor = color(230, 230, 0)
+  }
+
+  if(defor>180){
+    car.shapeColor = color(255, 0, 0)
+  }
+ }
+ drawSprites();
+}
